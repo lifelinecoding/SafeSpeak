0de2565577e861a8ce4c298e891d225168a6d3ed
lifelinecoding <apatel89329@gmail.com>
2026-07-10 15:04:29 +0530

    Feat: Implement database connection utility using mongoose

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("ALready connected to database!");
+    return;
+  }
+
+  try {
+    const connectionInstance = await mongoose.connect(
+      (process.env.MONGODB_URI as string) || "",
+      {},
+    );
+    connection.isConnected = connectionInstance.connections[0].readyState;
+    console.log(
+      "DB connected successfully",
+      connectionInstance.connection.host,
+    );
+  } catch (error) {
+    console.error("Database connection failed: ", error);
+    process.exit(1);
+  }
+}
+
+export { dbConnect };

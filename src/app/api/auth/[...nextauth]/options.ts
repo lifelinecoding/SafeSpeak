d06e7083d5e88f99798527ec39052dae8e705a90
lifelinecoding <apatel89329@gmail.com>
2026-07-13 14:48:40 +0530

    Feat: add username uniqueness check and verification code handling.
    -Renamed the middleware.ts file to proxy.ts according to next.js version 16
    
    -Fixed issue with resend email sending by installing @react-emails/resend package

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -13,7 +13,7 @@ export const authOptions: NextAuthOptions = {
         email: { label: "Email", type: "email", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
-
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       async authorize(credentials: any): Promise<any> {
         await dbConnect();
         try {
@@ -35,6 +35,7 @@ export const authOptions: NextAuthOptions = {
 
           if (!isPasswordCorrect) throw new Error("Incorrect Password");
           return user;
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (error: any) {
           throw new Error(error);
         }

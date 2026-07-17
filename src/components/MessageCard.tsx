b1376a36b7b00e1819bd9d1dc0c89377da9b8c91
lifelinecoding <apatel89329@gmail.com>
2026-07-17 14:11:26 +0530

    Feat: add MessageCard and AlertDialog components for message management with delete confirmation

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { Trash } from "lucide-react";
+import { Message } from "@/model/User";
+import { toast } from "sonner";
+
+type MessageCardProps = {
+  message: Message;
+  onMessageDelete: (messageId: string) => void;
+};
+
+// eslint-disable-next-line
+const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
+
+  //TODO: This method is pending to create after creating the route at the backend to delete.
+  function handleDeleteConfirm() {}
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+
+      <AlertDialog>
+        <AlertDialogTrigger render={<Button variant="outline" />}>
+          <Trash className="w-5 h-5" />
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete this
+              message from server.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={handleDeleteConfirm}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </Card>
+  );
+};
+
+export default MessageCard;
